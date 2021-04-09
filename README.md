3d128981f499c4569b188c7517f6e745e67f323a
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2021-04-08 21:14:40 -0300

    [refactor] extract URL

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # desmos-test
 Playing with code from https://jsbin.com/fekufuwiyi/edit
+
+## Things done
+
+- Extract URL
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let dict;
 
-fetch(
-  'https://gist.githubusercontent.com/dlants/d3b25b0f6c0bf8d023f65e86498bf9e6/raw/b310b5aff00f62f5073b3b8d366f5a639aa88ee3/3000-words.txt'
-).then(
+const URL = 'https://gist.githubusercontent.com/dlants/d3b25b0f6c0bf8d023f65e86498bf9e6/raw/b310b5aff00f62f5073b3b8d366f5a639aa88ee3/3000-words.txt'
+
+fetch(URL).then(
   (res) => res.text()
 ).then(
   (text) => {
